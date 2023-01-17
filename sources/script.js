3ff04dec154a25dc82f734ed943f90dffc97d106
KalazarDev <109695311+KalazarDev@users.noreply.github.com>
2023-01-17 19:25:35 -0300

    .

--- sources/script.js
+++ sources/script.js
@@ -7,38 +7,98 @@
 
 // Capturando dados do Html
 const button = document.querySelector("button");
-const vlr_ini = document.querySelector("#vlr_ini");
-const jur = document.querySelector("#jur");
-const tempo = document.querySelector("#tempo");
-
-
-// Capturando Evento de envio do Botão
-
-button.addEventListener("click", () => {
-    alert(`${vlr_ini.value} ${jur.value} ${tempo.value}`);
-})
+const buttonLimpar = document.querySelector("#limpar");
+// const vlr_ini = document.querySelector("#vlr_ini");
+// const jur = document.querySelector("#jur");
+// const tempo = document.querySelector("#tempo");
 
 
 // Adicionar Função
 
-var valor_inicial = 100
-var valor_juros = 1
-var periodo = 100
+var vlr_ini = 100
+var jur = 1
 var valor_corrigido = 0.00
+var tempo = 100
 
-for (let i = 1; i < periodo; i++) {
+function criaTabela() {
+    const criaTable = document.createElement("table")
+    const criaThead = document.createElement("thead")
+    const criaHead = document.createElement("th");
+    const criaHead2 = document.createElement("th");
+    const section = document.querySelector("section");
+    
+    section.appendChild(criaTable)
+    criaTable.appendChild(criaThead)
+    criaThead.appendChild(criaHead)
+    criaThead.appendChild(criaHead2)
+    criaHead.innerHTML = "DIA"
+    criaHead2.innerHTML = "VALOR"
 
-    if (valor_corrigido === 0.00) {
-        var juros =  valor_inicial * (valor_juros/100)
-        valor_corrigido += juros + valor_inicial;
-    }
-    else {
-        var juros =  valor_corrigido * (valor_juros/100)
-        valor_corrigido += juros;
-    }
-
-    console.log(`Dia ${i} ${parseFloat(valor_corrigido.toFixed(2))}`)
 
 }
 
+function Calcula_Juros() {
+for (let i = 1; i < tempo; i++) {
+
+    if (valor_corrigido === 0.00) {
+        var juros =  vlr_ini * (jur/100)
+        valor_corrigido += juros + vlr_ini;
+    }
+    else {
+        var juros =  valor_corrigido * (jur/100)
+        valor_corrigido += juros;
+    }
+
+    const tabela = document.querySelector("table")
+
+    const criaTbody = document.createElement("tbody")
+    const linha = document.createElement("tr");
+
+    const inserdias = document.createElement("td");
+    const inserdados = document.createElement("td");
+
+    const section = document.querySelector("section");
+
+    tabela.appendChild(criaTbody)
+    tabela.appendChild(linha);
+    linha.appendChild(inserdias)
+    linha.appendChild(inserdados);
+
+    inserdias.innerHTML =  `Dia ${i}`
+    inserdados.innerHTML = parseFloat(valor_corrigido.toFixed(2));
+
+}}
+
+// Capturando Evento de envio do Botão
+// console.log(Calcula_Juros())
+button.addEventListener("click", () => {
+    tbl = document.querySelector("table")
+    if ( document.getElementsByTagName("table").length > 0) {
+        var table = document.getElementsByTagName("table")[0];
+        table.parentNode.removeChild(table);
+    
+    } else {
+        criaTabela()
+        Calcula_Juros()
+    }
+
+  
+
+})
+
+
+buttonLimpar.addEventListener("click", () => {
+    tbl = document.querySelector("table")
+    if ( document.getElementsByTagName("table").length > 0) {
+        var table = document.getElementsByTagName("table")[0];
+        table.parentNode.removeChild(table);
+    
+    } else {
+        criaTabela()
+        Calcula_Juros()
+    }
+
+  
+
+})
 
